03b4238eb77e210b24c97b3577f08cf645bd140c
Dan Bovey <dan@danbovey.co.uk>
2015-02-04 14:37:00 +0000

    Fixed /messages

--- flatbook.js
+++ flatbook.js
@@ -4,4 +4,7 @@ $(function() {
 
 	// Move media above user content
 	// $('._4-u2').prepend()
+
+	// Add Flatbook credit
+	$('.rhcFooterWrap').append('<div class="rhcFooterCopyright"><span>Redesign by <a href="https://github.com/danbovey/Flatbook" target="_blank">Flatbook</a></span></div>');
 });
--- min/flatbook-min.js
+++ min/flatbook-min.js
@@ -1,1 +1,1 @@
-$(function(){$("._uaw").prepend('<button id="flatbook-hamburger">☰</button>')});
+$(function(){$("._uaw").prepend('<button id="flatbook-hamburger">☰</button>'),$(".rhcFooterWrap").append('<div class="rhcFooterCopyright"><span>Redesign by <a href="https://github.com/danbovey/Flatbook" target="_blank">Flatbook</a></span></div>')});
