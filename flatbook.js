76396e4039faa2727384e8a0f7bb9b5a45d40f09
Dan Bovey <dan@danbovey.co.uk>
2015-02-06 01:49:12 +0000

    Media moved above user content. Start of page stylings. Sidebar chat updated.

--- flatbook.js
+++ flatbook.js
@@ -3,7 +3,9 @@ $(function() {
 	$('._uaw').prepend('<button id="flatbook-hamburger">☰</button>');
 
 	// Move media above user content
-	// $('._4-u2').prepend()
+	$('._4-u2').each(function() {
+		$(this).find('.mtm').prependTo(this);
+	});
 
 	// Add Flatbook credit
 	$('.rhcFooterWrap').append('<div class="rhcFooterCopyright"><span>Redesign by <a href="https://github.com/danbovey/Flatbook" target="_blank">Flatbook</a></span></div>');
