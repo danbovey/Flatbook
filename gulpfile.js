d289c9016bec2cacd4d03a3dfe73cf806f369e45
Dan Bovey <dan@danbovey.uk>
2016-02-19 23:48:59 +0000

    Added messages, page, started options.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,15 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var notify = require('gulp-notify');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var rename = require('gulp-rename');
+var GulpKit = require('GulpKit');
 
-var runSequence = require('run-sequence');
+GulpKit(function(kit) {
+	kit.scss({
+		source: './scss/app.scss',
+		output: './chrome/css/flatbook.css'
+	});
 
-gulp.task('scss', function() {
-	return gulp.src('scss/app.scss')
-		.pipe(sourcemaps.init())
-		.pipe(sass())
-		.pipe(sourcemaps.write())
-		.pipe(rename('flatbook.css'))
-		.pipe(gulp.dest('chrome/css'));
-});
+	kit.scss({
+		source: './scss/options.scss',
+		output: './chrome/css'
+	});
 
-gulp.task('default', function() {
-	runSequence('scss');
-
-	gulp.watch('scss/**/*.scss', ['scss']);
+    kit.browserSync();
 });
